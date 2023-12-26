a7e8afb2693be85c96a47d349579f9dd0c6fbff5
Matthew Davis <matthew@matthewdavis.io>
2023-12-26 00:49:18 -0500

    bump

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export { externalClickHandler } from "./lib/external-click-handler";
+export { ModalConfig } from "./lib/modal-config";
+export { ModalInstance } from "./lib/modal-instance";
+export { ModalManager } from "./lib/modal-manager";
