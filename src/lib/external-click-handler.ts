7c66317f5f42e3d2f6b4ce600102b8f90db4da09
Matthew Davis <matthew@matthewdavis.io>
2023-12-27 14:05:22 -0500

    bump

--- src/lib/external-click-handler.ts
+++ src/lib/external-click-handler.ts
@@ -12,7 +12,7 @@ export const externalClickHandler = (child: HTMLElement) => {
 		console.log(child);
 		console.log(event);
 		console.log(event.srcElement.attributes["data-select-item"]);
-		if (child && !child.contains(event.target) && event.srcElement.localName !== "button" && event.srcElement.getAttribute("data-select-item") != "") {
+		if (child && !child.contains(event.target) && event.srcElement.localName !== "button" && event.srcElement.getAttribute("data-select-item") != "" && event.srcElement.getAttribute("data-select-item") != undefined) {
 			child.dispatchEvent(new CustomEvent("externalClickEvent", { detail: event }));
 		}
 	};
